4209abd41ce9105a1054992d20dd3a10d2a66bfe
슬 이 <blacksangi@gmail.com>
2016-03-04 00:51:16 +0900

    Basic_C second

---
+++ Basic_C/HelloWorld/HelloWorld/Escape.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\t Hello C! \n Hello world! \n");
+	printf("큰따옴표: \" \" \n");
+	printf("작은따옴표: \' \' \n");
+	printf("역슬래시: \\ \n");
+
+	return 0;
+}
--- Basic_C/HelloWorld/HelloWorld/Hello.c
+++ Basic_C/HelloWorld/HelloWorld/Hello.c
@@ -1,6 +1,12 @@
+/*
+	파일명: Hello.c
+	만든이: seul lee
+	프로그램 내용: Hello C World 테스트
+*/
+
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	printf("Hello C World \n");
 
